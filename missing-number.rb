798c7119c17fa322c24d07493f22cac8d40c9a66
nimplay <nimrod7day@gmail.com>
2022-10-02 15:11:47 -0400

    missing number

---
+++ missing-number.rb
@@ -0,0 +1,13 @@
+# Given an array nums containing n distinct numbers in the range [0, n],
+# return the only number in the range that is missing from the array.
+def missing_number(nums)
+  sum = 0
+  total = 0
+  nums.each_with_index do |element, index|
+     sum += element
+     total += index + 1
+  end
+  puts total - sum
+end
+
+missing_number([3, 0, 1])
